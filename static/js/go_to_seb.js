22806ba405c94225fd73ea4c94cd719399caaaac
truong51972 <tranquoctruong20@gmail.com>
2024-10-17 17:06:40 +0700

    small fix

--- static/js/go_to_seb.js
+++ static/js/go_to_seb.js
@@ -1,5 +1,5 @@
 const seb_link = document.getElementById('start_assign_btn').getAttribute("sebLink");
 const hostname = window.location.hostname
 document.getElementById('start_assign_btn').addEventListener('click', function() {
-    window.location.href = `seb://https://${hostname}${seb_link}`;
+    window.location.href = `seb://${hostname}${seb_link}`;
 });
